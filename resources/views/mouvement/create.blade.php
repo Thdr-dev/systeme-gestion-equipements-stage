523902a7265ca96a831ee185c14b12e9c2e2741b
DerkaouiTaha <tahaderkaoui26@gmail.com>
2026-03-26 20:14:21 +0100

    changement

--- resources/views/mouvement/create.blade.php
+++ resources/views/mouvement/create.blade.php
@@ -92,3 +92,26 @@
     </div>
 
 @endsection
+
+@if(Auth::user()->isAdmin)
+
+    @section("scripts")
+
+        <script>
+            const typeSelect = document.querySelector('select[name="type"]');
+            const uniteDiv = document.querySelector('select[name="to_unite_id"]').parentElement;
+
+            typeSelect.addEventListener('change', function() {
+                if (this.value === 'Transfert') {
+                    uniteDiv.style.display = 'block';
+                } else {
+                    uniteDiv.style.display = 'none';
+                }
+            });
+
+            window.onload = () => typeSelect.dispatchEvent(new Event('change'));
+        </script>
+
+    @endsection
+
+@endif
